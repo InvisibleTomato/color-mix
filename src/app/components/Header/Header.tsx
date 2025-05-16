3f54724877760ceed9612974c9c034b756760c50
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-16 20:00:42 +0900

    ✨ h1タグをLinkコンポーネントでwrap

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "@/app/components/Header/header.module.css";
+import Link from "next/link";
 
 export const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <h1>Color Mix</h1>
+      </Link>
+    </div>
+  );
 };
