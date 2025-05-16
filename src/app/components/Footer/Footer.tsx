e262042bd727bc89317276d06b131614b8b14a01
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-16 12:07:02 +0900

    ✨ Footerコンポーネントのフォルダ、ファイルとcssを用意

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "@/app/components/Footer/footer.module.css";
+
+export const Footer = () => {
+  return <div className={styles.footer}>Footer</div>;
+};
