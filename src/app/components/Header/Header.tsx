ab46c91020fe355a4007082e0f727966b6fadd51
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-16 12:06:51 +0900

    ✨ Headerコンポーネントのフォルダ、ファイルとcssを用意

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "@/app/components/Header/header.module.css";
+
+export const Header = () => {
+  return <div className={styles.header}>Header</div>;
+};
