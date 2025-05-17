4d74903b45352724aa9c93babcc2c6aab293aabd
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-17 17:54:22 +0900

    ♻️ default export に変更

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styles from "./signin.module.css";
+import Link from "next/link";
 
-export const SignIn = () => {
-  return <div>SignIn</div>;
+const SignIn = () => {
+  return (
+    <div>
+      <h2>Color Mix</h2>
+      <div className={styles.signin}>
+        <p>メールアドレス</p>
+        <input type="text" placeholder="example@email.com" />
+        <p>パスワード</p>
+        <input type="text" placeholder="123abc" />
+        <button>サインイン</button>
+      </div>
+      <p>アカウントをお持ちでない方</p>
+      <button>Googleでログイン</button>
+      <p>または</p>
+      <Link href={"/home"}>ゲストモードで使う</Link>
+    </div>
+  );
 };
+
+export default SignIn;
