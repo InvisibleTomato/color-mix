dd9ac198c326fa6432adae349b336597bec39324
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-17 16:57:01 +0900

    💄 メインフォントとしてnoto-sansとrobotoを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,25 @@
-import type { Metadata } from "next";
 import "./reset.css";
 import "./globals.css";
 import styles from "./layout.module.css";
 import { Header } from "@/app/components/Header/Header";
 import { Footer } from "@/app/components/Footer/Footer";
+import { Noto_Sans_JP, Roboto } from "next/font/google";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Color Mix",
-  description: "美容師向けのカラー剤比率計算アプリ",
+  description: "美容師向けのカラー剤調合計算アプリ",
 };
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-roboto",
+});
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-noto-sans-jp",
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={`${roboto.variable} ${notoSansJP.variable}`}>
       <body className={styles.body}>
         <Header />
 
