f1c958571d73fcc7938aa8cabfe7506e8df197ae
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-17 16:56:18 +0900

    💄 フォントをoutfitに変更

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "@/app/components/Header/header.module.css";
 import Link from "next/link";
+import { Outfit } from "next/font/google";
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-outfit",
+});
 
 export const Header = () => {
   return (
     <div className={styles.header}>
       <Link href="/">
-        <h1>Color Mix</h1>
+        <h1 className={outfit.className}>Color Mix</h1>
       </Link>
     </div>
   );
