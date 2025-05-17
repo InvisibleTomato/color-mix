8a6bcad03cabf2d6a25b276a618081854124c803
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-17 16:58:00 +0900

    💄 SignInコンポーネントを配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from "@/app/page.module.css";
+import { SignIn } from "@/app/signin/page";
 
 export default function Home() {
   return (
     <div>
-      <h2 className={styles.main}>Hello, World!!</h2>
+      <h2 className={styles.main}>
+        <SignIn />
+      </h2>
     </div>
   );
 }
