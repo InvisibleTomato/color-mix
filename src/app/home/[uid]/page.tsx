41b4bf607cf4b42465bb97c66324fb773aec94f0
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-20 13:00:17 +0900

    ♻️ useParams()を使用してuidを読み込む形に修正

--- src/app/home/[uid]/page.tsx
+++ src/app/home/[uid]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-interface PageProps {
-  params: { uid: string };
-}
+import { useParams } from "next/navigation";
 
-export default function HomePage({ params }: PageProps) {
+export default function HomePage() {
+  const params = useParams();
+  const uid = params.uid as string;
   return (
     <div>
-      <h1>ようこそ、ユーザー {params.uid} さん</h1>
+      <h1>ようこそ、ユーザー {uid} さん</h1>
       {/* ユーザーごとのデータをFirestoreから読み込むなど */}
     </div>
   );
