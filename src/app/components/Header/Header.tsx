7b479795713a5df76b31a7625b5cc0f88a782b1f
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-22 06:38:34 +0900

    ♻️ 🎨 ロゴをコンポーネント化

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import styles from "@/app/components/Header/header.module.scss";
 import Link from "next/link";
-import { Outfit } from "next/font/google";
-const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: "--font-outfit",
-});
+
+import Logo from "../Logo/Logo";
 
 export const Header = () => {
   return (
     <div className={styles.header}>
       <Link href="/">
-        <h1 className={`${outfit.className} ${styles.logo}`}>Color Mix</h1>
+        <Logo />
       </Link>
     </div>
   );
