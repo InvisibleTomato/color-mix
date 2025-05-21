bdaeb0f1acffb5831d1c2401ab6d79293be85212
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-22 05:12:12 +0900

    ✨ パスワード再設定用のページを作成

--- src/app/password-reset/page.tsx
+++ src/app/password-reset/page.tsx
@@ -2,14 +2,18 @@
 import { auth } from "@/app/lib/firebase/auth";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { useState } from "react";
+import styles from "./password-reset.module.scss";
 
 const PasswordReset = () => {
   const [email, setEmail] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    //メールアドレスが取得できているか確認
+    console.log("入力されたメールアドレス:", email);
     const actionCodeSettings = {
       // パスワード再設定後のリダイレクト URL
+      //↓デプロイ時は本番環境のURLに変更する
       url: "http://localhost:3000/",
     };
     try {
@@ -21,34 +25,24 @@ const PasswordReset = () => {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-6 text-center font-noto-sans">
-          パスワードリセット
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
+    <div>
+      <div className={styles.passwordResetContainer}>
+        <h2 className={styles.passwordResetTitle}>パスワードリセット</h2>
+        <form onSubmit={handleSubmit} className={styles.passwordResetForm}>
           <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-700 font-noto-sans"
-            >
-              メールアドレス
-            </label>
+            <label htmlFor="email">メールアドレス</label>
             <input
               type="email"
               id="email"
               name="email"
               required
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="example@example.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className={styles.passwordResetInput}
             />
           </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 font-noto-sans disabled:opacity-50 disabled:cursor-not-allowed"
-          >
+          <button type="submit" className={styles.passwordResetButton}>
             リセットリンクを送信
           </button>
         </form>
