5ba0413ed736452449f1c9bd1a76b75a2c7a5e1d
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-23 05:54:16 +0900

    🚧 Historyコンポーネントのリストを作成

--- src/app/components/History/History.tsx
+++ src/app/components/History/History.tsx
@@ -1,7 +1,46 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import styles from "./History.module.scss";
+import { useState } from "react";
 
 const History = () => {
-  return <div>History</div>;
+  const [isHistoryOpen, setIsHistoryOpen] = useState(false);
+
+  const handleHistoryClick = () => {
+    setIsHistoryOpen((prev) => !prev);
+  };
+  return (
+    <div>
+      <div className={styles.HistoryIconContainer}>
+        <span onClick={handleHistoryClick}>
+          <Image
+            src="/history-icon.svg"
+            width={40}
+            height={40}
+            alt="Picture of the author"
+          />
+        </span>
+      </div>
+      {/* ポップアップとしてのプロフィール表示 */}
+      {isHistoryOpen && (
+        <div className={styles.historyPopup}>
+          <div className={styles.historyPopupContent}>
+            <h3 className={styles.historyPopupTitle}>計算履歴</h3>
+            <div className={styles.historyPopupList}>
+              <ul>
+                <li>計算履歴1</li>
+                <li>計算履歴2</li>
+                <li>計算履歴3</li>
+                <li>計算履歴4</li>
+                <li>計算履歴5</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default History;
