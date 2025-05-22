4fa38671457a8900e8f3489bb21b3c52f90f5bfe
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-22 11:12:14 +0900

    🔥 ロゴのクリックイベントを削除

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -1,10 +1,5 @@
-"use client";
-import React from "react";
 import styles from "@/app/components/Logo/Logo.module.scss";
-import { useRouter } from "next/navigation";
-import { auth } from "@/app/lib/firebase/auth";
 import { Outfit } from "next/font/google";
-import { User } from "firebase/auth";
 
 //ロゴのフォント
 const outfit = Outfit({
@@ -14,24 +9,11 @@ const outfit = Outfit({
 });
 
 // ロゴコンポーネント
+
 const Logo = () => {
-  const router = useRouter();
-  // ロゴをクリックしたときの処理
-  const handleClick = async () => {
-    const currentUser: User | null = auth.currentUser;
-    console.log("ユーザーのID" + currentUser);
-
-    if (currentUser) {
-      const uid = currentUser.uid;
-      router.push(`/home/${uid}`);
-    } else {
-      router.push("/");
-    }
-  };
-
   return (
     <div>
-      <div onClick={handleClick}>
+      <div>
         <h1 className={`${outfit.className} ${styles.logo}`}>Color Mix</h1>
       </div>
     </div>
