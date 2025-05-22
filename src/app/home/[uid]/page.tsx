d4888e12b3962b1c13437c2128767ec641323972
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-22 11:27:33 +0900

    💄 Home[uid]の雛形を作成

--- src/app/home/[uid]/page.tsx
+++ src/app/home/[uid]/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import Profile from "@/app/components/Profile/Profile";
-import { useParams } from "next/navigation";
+import History from "@/app/components/History/History";
+import styles from "./home-uid.module.scss";
 
 export default function HomePage() {
-  const params = useParams();
-  const uid = params.uid as string;
   return (
     <div>
-      <h1>ようこそ、ユーザー {uid} さん</h1>
-      <Profile />
+      <div className={styles.homeContainer}>
+        <History />
+        <Profile />
+      </div>
     </div>
   );
 }
