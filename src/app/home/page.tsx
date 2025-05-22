ab89a9051121402407968f085bed205ccd5aacaa
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-23 05:31:45 +0900

    🔥 通常のHomeからHistoryコンポーネントを削除

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,11 @@
 // src/app/home/page.tsx
 import styles from "./home.module.scss";
 import Profile from "@/app/components/Profile/Profile";
-import History from "@/app/components/History/History";
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <Profile />
-      {/* 履歴コンポーネント */}
-      <History />
     </div>
   );
 };
