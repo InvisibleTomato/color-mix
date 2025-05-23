ca6336a2295b02ecda00aae27dc391ffb3a945d1
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-23 12:42:09 +0900

    🎨 全体をwrapするdivを作成

--- src/app/components/History/History.tsx
+++ src/app/components/History/History.tsx
@@ -12,33 +12,35 @@ const History = () => {
   };
   return (
     <div>
-      <div className={styles.HistoryIconContainer}>
-        <span onClick={handleHistoryClick}>
-          <Image
-            src="/history-icon.svg"
-            width={40}
-            height={40}
-            alt="Picture of the author"
-          />
-        </span>
-      </div>
-      {/* ポップアップとしてのプロフィール表示 */}
-      {isHistoryOpen && (
-        <div className={styles.historyPopup}>
-          <div className={styles.historyPopupContent}>
-            <h3 className={styles.historyPopupTitle}>計算履歴</h3>
-            <div className={styles.historyPopupList}>
-              <ul>
-                <li>計算履歴1</li>
-                <li>計算履歴2</li>
-                <li>計算履歴3</li>
-                <li>計算履歴4</li>
-                <li>計算履歴5</li>
-              </ul>
+      <div className={styles.HistoryContainer}>
+        <div className={styles.HistoryIconContainer}>
+          <span onClick={handleHistoryClick}>
+            <Image
+              src="/history-icon.svg"
+              width={40}
+              height={40}
+              alt="Picture of the author"
+            />
+          </span>
+        </div>
+        {/* ポップアップとして履歴表示 */}
+        {isHistoryOpen && (
+          <div className={styles.historyPopup}>
+            <div className={styles.historyPopupContent}>
+              <h3 className={styles.historyPopupTitle}>計算履歴</h3>
+              <div className={styles.historyPopupList}>
+                <ul>
+                  <li>計算履歴1</li>
+                  <li>計算履歴2</li>
+                  <li>計算履歴3</li>
+                  <li>計算履歴4</li>
+                  <li>計算履歴5</li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
