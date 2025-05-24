bda4ea02b84f53936cc5c401e60c06d531b96b80
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-25 02:32:40 +0900

    💄 Calcコンポーネントを追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,15 @@
 // src/app/home/page.tsx
 import styles from "./home.module.scss";
 import Profile from "@/app/components/Profile/Profile";
+import Calc from "../components/Calc/Calc";
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <Profile />
+      <div className={styles.calcContainer}>
+        <Calc />
+      </div>
     </div>
   );
 };
