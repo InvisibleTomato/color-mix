a57bd72c88931f45793795c06de1f43e11d9ccd2
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-25 02:33:13 +0900

    🚧 💄 Calcコンポーネント作成中(UI)

--- src/app/components/Calc/Calc.tsx
+++ src/app/components/Calc/Calc.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import styles from "./Calc.module.scss";
 
 const Calc = () => {
-  return <div>Calc</div>;
+  return (
+    <div>
+      <div className={styles.calcInputContainer}>
+        <table className={styles.inputTable}>
+          <thead>
+            <tr>
+              <th>薬剤</th>
+              <th>比率</th>
+              <th>%</th>
+              <th>量(g)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[
+              { name: "A剤", ratio: 1, percent: "", amount: "30g" },
+              { name: "B剤", ratio: 1, percent: "", amount: "30g" },
+              { name: "C剤", ratio: 2, percent: "", amount: "60g" },
+              { name: "D剤", ratio: "", percent: 10, amount: "3.0g" },
+            ].map((row, index) => (
+              <tr key={index}>
+                <td>{row.name}</td>
+                <td>
+                  <input
+                    type="number"
+                    defaultValue={row.ratio}
+                    className={styles.inputCell}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    defaultValue={row.percent}
+                    className={styles.inputCell}
+                  />
+                </td>
+                <td className={styles.amountCell}>{row.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Calc;
