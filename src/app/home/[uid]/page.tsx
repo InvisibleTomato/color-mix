c584b494bf450b6ce08de68e18d99eb2eacd99e7
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-26 18:07:11 +0900

    🎨 更新のための処理を追加

--- src/app/home/[uid]/page.tsx
+++ src/app/home/[uid]/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 import LoginProfile from "@/app/components/LoginProfile/LoginProfile";
+import { useRef } from "react";
 import History from "@/app/components/History/History";
 import styles from "./home-uid.module.scss";
 import UsersCalc from "@/app/components/UsersCalc/UsersCalc";
+import type { HistoryRef } from "@/app/components/History/History";
 
 export default function HomePage() {
+  const historyRef = useRef<HistoryRef>(null);
+
+  const handleSaved = () => {
+    historyRef.current?.reload();
+  };
   return (
     <div>
       <div className={styles.homeContainer}>
         <div className={styles.homeIconContainer}>
-          <History />
+          <History ref={historyRef} />
           <LoginProfile />
         </div>
         <div className={styles.calcContainer}>
-          <UsersCalc />
+          <UsersCalc onSaved={handleSaved} />
         </div>
       </div>
     </div>
