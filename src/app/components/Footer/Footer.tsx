d7b7e985ed1671deca4080f6c99abdb31302ae49
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-28 14:58:32 +0900

    importのパスを修正

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "@/app/components/Footer/footer.module.scss";
+import styles from "@/app/components/Footer/Footer.module.scss";
 
 export const Footer = () => {
   return (
