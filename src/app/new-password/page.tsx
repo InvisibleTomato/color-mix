0c56a7ea758d76c2e200dbf7765769d59fccdf8e
InvisibleTomato <invisibletomato80@gmail.com>
2025-05-28 16:57:00 +0900

    🚑 useSearchParams の使用に伴うビルドエラーを Suspense 導入で解消

--- src/app/new-password/page.tsx
+++ src/app/new-password/page.tsx
@@ -1,8 +1,12 @@
 import NewPassword from "./NewPasswordClient";
-
+import { Suspense } from "react";
 // 動的レンダリング指定
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <NewPassword />;
+  return (
+    <Suspense fallback={<div>読み込み中...</div>}>
+      <NewPassword />
+    </Suspense>
+  );
 }
